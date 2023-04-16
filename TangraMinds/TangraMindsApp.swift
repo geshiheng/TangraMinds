3867f5d3f58d650dac7886df01aab3ec43898aae
geshiheng <ge952893553@gmail.com>
2023-04-15 17:46:19 -0700

    Initial Commit

---
+++ TangraMinds/TangraMindsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TangraMindsApp.swift
+//  TangraMinds
+//
+//  Created by Shiheng Ge on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TangraMindsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
